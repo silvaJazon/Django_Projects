0c1dbd02568fd09f132ca3f0a1a17db3c43c4129
silvaJazon <jsilva@d-lavro.com.br>
2023-12-03 11:18:54 -0300

    Removed unwanted files.

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,4 +1,4 @@
-"""Django_Projects URL Configuration
+"""projeto URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
