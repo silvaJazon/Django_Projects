9ded0cd7761f1f0852c075a34a734f5fef726a3a
Jazon <jsilva@d-lavro.com.br>
2023-12-06 23:30:02 +0000

    Lesson 12 Framework in

--- Dajngo_Framework_na_Pratica/usuarios/models.py
+++ Dajngo_Framework_na_Pratica/usuarios/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager,
+    AbstractBaseUser,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class usuario(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(
+        verbose_name="Email do Usuário",
+        max_length=194,
+        unique=True,
+    )
+
+    is_active = models.BooleanField(
+        verbose_name="Usuário está Ativo",
+        default=True,
+    )
+
+    is_staff = models.BooleanField(
+        verbose_name="Usuário e um Dev",
+        default=False,
+    )
+
+    is_superuser = models.BooleanField(
+        verbose_name="Usuario é um Superuser",
+        default=False,
+    )
+
+    USERNAME_FIELD = "email"
+
+    class Meta:
+        verbose_name = "Usuário",
+        verbose_name_plural = "Usuários"
+        db_table = "usuario"
+
+    def __str__(self):
+        return self.email
