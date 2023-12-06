df68e301c1252c6f0d4186dfdca60c87a9a70eff
Jazon <jsilva@d-lavro.com.br>
2023-12-06 23:51:38 +0000

    Lesson 12 Framework in

--- Dajngo_Framework_na_Pratica/usuarios/models.py
+++ Dajngo_Framework_na_Pratica/usuarios/models.py
@@ -6,7 +6,41 @@ from django.contrib.auth.models import (
 )
 
 
-class usuario(AbstractBaseUser, PermissionsMixin):
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None):
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+
+        user.is_active = True
+        user.is_staff = False
+        user.is_superuser = False
+
+        if password:
+            user.set_password(password)
+
+        user.save()
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(
+            email=self.normalize_email(email),
+            password=password
+        )
+
+        user.is_active = True
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.set_password(password)
+
+        user.save()
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         verbose_name="Email do Usuário",
         max_length=194,
@@ -29,6 +63,8 @@ class usuario(AbstractBaseUser, PermissionsMixin):
     )
 
     USERNAME_FIELD = "email"
+
+    objects = UserManager
 
     class Meta:
         verbose_name = "Usuário",
